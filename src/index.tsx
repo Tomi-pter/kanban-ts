319245431e144c851f50fc04d5007e83bfe0b437
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-11-15 12:44:29 +0100

    downgraded to react 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store, { persistor } from './store/store';
 import { Provider } from 'react-redux';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { PersistMigrate } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
